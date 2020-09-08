5f7518b28b2961af742e7b8353dd1e3830e4f6b3
Bao Khanh Dinh <khanhdb@sandinh.net>
2020-09-08 17:39:42 +0700

    refactor

--- app/services/CommandExecutor.scala
+++ app/services/CommandExecutor.scala
@@ -19,17 +19,23 @@ trait CommandExecutor {
 @Singleton
 class AtomicCommandExecutor extends CommandExecutor {
   override def execute(): Unit = {
+    runCommand("/bin/bash", "/home/khanhdb/docker/build-image.sh", "re-build image from latest code...", 4)
+    runCommand("docker-compose", "-f /home/khanhdb/docker/docker-compose.yml up -d", "docker-compose up...", 4)
+  }
+
+  private def runCommand(command : String, param : String, startLog : String, maxEmptyLine: Int): Unit = {
     import java.io.{BufferedReader, InputStreamReader}
-    logger.debug("executing...")
-    val p = new ProcessBuilder("/bin/bash","/home/khanhdb/docker/deploy.sh")
+    logger.debug(startLog)
+    val p = new ProcessBuilder(command, param)
     val p2 = p.start()
-    val br = new BufferedReader(new InputStreamReader(p2.getInputStream()))
+    val br = new BufferedReader(new InputStreamReader(p2.getInputStream))
 
     var line : String = ""
     var nullLineCount = 0
-    while ({line = br.readLine(); nullLineCount < 4}) {
+    while ({line = br.readLine(); nullLineCount < maxEmptyLine}) {
       nullLineCount = if (line == null) nullLineCount + 1 else nullLineCount
       logger.debug(line)
     }
+    logger.debug("done")
   }
 }
