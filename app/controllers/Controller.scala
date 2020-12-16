10fe194b30d07125854e0a573e96355a0d5693e6
Bao Khanh Dinh <khanhdb@sandinh.net>
2020-12-16 15:12:12 +0700

    error handling

--- app/controllers/Controller.scala
+++ app/controllers/Controller.scala
@@ -11,12 +11,17 @@ class Controller @Inject()(cc: ControllerComponents,
                            commandExecutor: CommandExecutor) extends AbstractController(cc) {
 
   def execute: Action[JsValue]= Action(parse.json) { request =>
-    val command = request.body("command").as[String]
-    val secret = request.body("secret").as[String]
-    if (secret == sys.env("EXEC_SECRET")){
-      val result = commandExecutor.execute(command)
-      Ok(result)
-    } else Unauthorized("EXEC_SECRET doesn't matched")
+    try{
+      val command = request.body("command").as[String]
+      val secret = request.body("secret").as[String]
+      if (secret == sys.env("EXEC_SECRET")){
+        val result = commandExecutor.execute(command)
+        Ok(result)
+      } else Unauthorized("EXEC_SECRET doesn't matched")
+    } catch {
+      case _ : NoSuchElementException =>
+        BadRequest("invalid body format or server has missing config")
+    }
   }
 
 }
