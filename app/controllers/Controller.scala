c6f1b9db1168a591c8c610a6301ae8fc00ba4690
Bao Khanh Dinh <khanhdb@sandinh.net>
2021-02-01 11:17:41 +0700

    add secret to application.conf

--- app/controllers/Controller.scala
+++ app/controllers/Controller.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import play.api.{Configuration, Logger}
 import play.api.libs.json.JsValue
 
 import javax.inject._
@@ -8,16 +9,18 @@ import services.CommandExecutor
 
 @Singleton
 class Controller @Inject()(cc: ControllerComponents,
-                           commandExecutor: CommandExecutor) extends AbstractController(cc) {
+                           commandExecutor: CommandExecutor, config : Configuration) extends AbstractController(cc) {
 
+  protected val logger: Logger = Logger(this.getClass)
   def execute: Action[JsValue]= Action(parse.json) { request =>
     try{
       val command = request.body("command").as[String]
       val secret = request.body("secret").as[String]
-      if (secret == sys.env("EXEC_SECRET")){
+      val secretInConfig = config.get[String]("secret")
+      if (secret == secretInConfig){
         val result = commandExecutor.execute(command)
         Ok(result)
-      } else Unauthorized("EXEC_SECRET doesn't matched")
+      } else Unauthorized("secret doesn't matched")
     } catch {
       case _ : NoSuchElementException =>
         BadRequest("invalid body format or server has missing config")
